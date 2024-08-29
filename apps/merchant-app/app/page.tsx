f649047f7f3660ad81d15175986fbd7d1046c817
Narayan-21 <naaidjan.19@gmail.com>
2024-08-30 03:18:55 +0530

    init

---
+++ apps/merchant-app/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div className="text-4xl">
+      Hi there from merchant app
+    </div>
+  );
+}
---
+++ apps/user-app/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div className="text-xl">
+        Hi there
+    </div>
+  );
+}
