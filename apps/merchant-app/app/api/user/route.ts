7c1a92e833c7499ca82bace809eec00778d47fc4
Narayan-21 <naaidjan.19@gmail.com>
2024-08-31 20:13:56 +0530

    final fix to the starter repo

--- apps/merchant-app/app/api/user/route.ts
+++ apps/merchant-app/app/api/user/route.ts
@@ -1,13 +1,15 @@
 import { NextResponse } from "next/server";
-import PrismaClient from "@repo/db/client";
+import client from "@repo/db/client";
 
-const client = new PrismaClient();
+// const client = new PrismaClient();
 
 export const GET = async() => {
     await client.user.create({
         data: {
             email: "asd",
-            name: "adads"
+            name: "adads",
+            number: "asdaf",
+            password: "pass"
         }
     })
     return NextResponse.json({
--- apps/merchant-app/app/lib/auth.ts
+++ apps/merchant-app/app/lib/auth.ts
@@ -1,9 +1,9 @@
-import Google from "next-auth/providers/google";
+import GoogleProvider from "next-auth/providers/google";
 import db from "@repo/db/client";
 
 export const authOptions = {
     providers: [
-        Google({
+      GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID || "",
             clientSecret: process.env.GOOGLE_CLIENT_SECRET || ""
         })
