320dbab2bb4fadb9a94bab36b44fd3539962ffd6
Narayan-21 <naaidjan.19@gmail.com>
2024-09-02 20:25:50 +0530

    p2p server actions

---
+++ apps/user-app/app/(dashboard)/p2p/page.tsx
@@ -0,0 +1,8 @@
+import { SendCard } from "../../../components/SendCard";
+
+export default function() {
+    return <div className="w-full">
+        P2P Dashboard
+        <SendCard />
+    </div>
+}
